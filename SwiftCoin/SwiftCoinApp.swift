d0a4f4aec7bffe755d297df7be1b3c39d3e3174d
Andre Bauer <andrebauer@live.com>
2022-11-04 22:53:01 +0900

    init

---
+++ SwiftCoin/SwiftCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCoinApp.swift
+//  SwiftCoin
+//
+//  Created by Andre Bauer on 2022/11/04.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
