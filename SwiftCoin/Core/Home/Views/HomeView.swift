3bed46540c1c931bef089569939cb46ce71f3ed7
Andre Bauer <andrebauer@live.com>
2022-11-07 11:20:48 +0900

    models and views

---
+++ SwiftCoin/Core/Home/Views/HomeView.swift
@@ -0,0 +1,24 @@
+//
+//  HomeView.swift
+//  SwiftCoin
+//
+//  Created by Andre Bauer on 2022/11/07.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        NavigationView{
+            ScrollView(.vertical, showsIndicators: false) {
+                <#code#>
+            }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
