e13421e6bfef99b00f1d2351e0a7ea8b69354359
Andre Bauer <andrebauer@live.com>
2022-11-10 15:32:53 +0900

    home view model

---
+++ SwiftCoin/Core/Home/ViewModels/HomeViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewModel.swift
+//  SwiftCoin
+//
+//  Created by Andre Bauer on 2022/11/10.
+//
+
+import SwiftUI
+
+struct HomeViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeViewModel()
+    }
+}
